e211140996ff2d4ee3b2080bf58805737735fba7
thisfikri <32200380+thisfikri@users.noreply.github.com>
2019-10-07 11:08:48 +0700

    test

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 import { Content, Item, Input, Label } from "native-base";
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 class App extends Component {
   constructor(props) {
