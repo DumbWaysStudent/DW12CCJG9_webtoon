6eafc2ae254c6594522c3dc2c447714c4330f95b
thisfikri <32200380+thisfikri@users.noreply.github.com>
2019-10-10 13:19:29 +0700

    add edit profile screen

--- android/app/src/screens/OnAuth/EditProfile.js
+++ android/app/src/screens/OnAuth/EditProfile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, StyleSheet, SafeAreaView, TouchableOpacity, Image} from 'react-native';
 import  { Text } from 'native-base'
 import Icon from "react-native-vector-icons/FontAwesome5";
+import ImagePicker from 'react-native-image-picker';
 
 class Profile extends Component {
   constructor(props) {
@@ -11,31 +12,59 @@ class Profile extends Component {
     };
   }
 
+  imagePickerHandler() {
+    const options = {
+      title: 'Select Avatar',
+      customButton: [{
+        name: 'fb',
+        title: 'Choose Photo From Facebook'
+      }],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images'
+      }
+    }
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response =', response);
+
+      if (response.didCancel) {
+        console.log('User Cancelled image picker')
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error)
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton)
+      } else {
+        const source = { uri: response.uri }
+
+        this.setState({
+          profilePicture: source
+        })
+      }
+    })
+  }
+
   render() {
     return (
       <SafeAreaView style={styles.container}>
           <View style={styles.header}>
 
             <View style={styles.headerTitle}>
-              <Text style={styles.headerTitleText}>Profile</Text>
+              <Text style={styles.headerTitleText}>Edit Profile</Text>
             </View>
 
             <TouchableOpacity
+              onPress={() => this.props.navigation.navigate('Profile')}
               style={styles.headerEditBtn}>
               <Icon name="check" size={23} />
             </TouchableOpacity>
 
           </View>
-          <View style={styles.profilePicture}>
-              <Image source={this.state.profilePicture} />
-            </View>
-            <View style={styles.options}>
-              <TouchableOpacity style={styles.optionsItem}>
-                <Text style={styles.optionsText}>My Webtoon Creation <Icon name="angle-right" size={20} /></Text>
+            <View style={styles.profilePicture}>
+              <Image large source={this.state.profilePicture} style={styles.profilePictureImage} />
+              <TouchableOpacity onPress={()=> this.imagePickerHandler()}>
+                <Icon name="camera" />
               </TouchableOpacity>
-              <TouchableOpacity style={styles.optionsItem}>
-                <Text style={styles.optionsText}>Log Out</Text>
-              </TouchableOpacity>
+              <Text style={styles.profileName}>Fikri Haikal</Text>
             </View>
       </SafeAreaView>
     )
@@ -67,6 +96,28 @@ const styles = StyleSheet.create({
     flexDirection: 'row-reverse',
     padding: 6
   },
+  profilePicture: {
+    alignItems: 'center',
+    paddingVertical: 15
+  },
+  profilePictureImage: {
+    width: 140,
+    height: 140 ,
+    borderRadius: 140/2
+  },
+  profileName: {
+    fontWeight: 'bold',
+    padding: 8
+  },
+  optionsItem: {
+    borderBottomWidth: 2,
+    borderColor: '#444',
+    paddingVertical: 20,
+    paddingHorizontal: 10
+  },
+  optionsItemTopBorder: {
+    borderTopWidth: 2,
+  }
 })
 
 export default Profile;
